11f5f1845de26dd4dd2e4fecdfd1160bc8bed41f
kbauch <karine.bauch.web@gmail.com>
2024-09-16 19:09:08 +0200

    update router after refactoring services and add router information

--- src/api/router.py
+++ src/api/router.py
@@ -1,6 +1,8 @@
+import typing
+
 import fastapi
 
-import services.meteo
+import services.recipe
 
 app = fastapi.FastAPI()
 
@@ -10,7 +12,14 @@ def root() -> dict:
     return {"message": "Welcome to the new CooKin app !"}
 
 
-@app.post("/{location}")
-def get_recipe(location: str) -> dict:
-    response = services.meteo.get_weather(location)
+@app.post(
+    "/{location}",
+    description="Get an original recipe adapted to weather condition of the location",
+    status_code=fastapi.status.HTTP_200_OK,
+    response_model=str,
+    # TODO: add error responses=
+)
+async def get_recipe(location: str) -> str:
+    response = services.recipe.get_recipe(location)
+    print(response)
     return response
