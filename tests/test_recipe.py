2fddf6f516054af9c240888f0c8d2a436b3d8f82
kbauch <karine.bauch.web@gmail.com>
2024-09-17 17:40:19 +0200

    update test_weather

--- tests/test_recipe.py
+++ tests/test_recipe.py
@@ -14,4 +14,9 @@ def test_recipe():
 def test_weather():
     weather = services.weather.get_weather("London")
     assert isinstance(weather, dict)
+    assert weather["nearest_area"][0]["country"][0]["value"]
+    assert weather["current_condition"][0]["weatherDesc"][0]["value"]
+    assert weather["current_condition"][0]["temp_C"]
+    assert weather["current_condition"][0]["humidity"]
+    assert weather["current_condition"][0]["windspeedKmph"]
     assert weather["nearest_area"][0]["country"][0]["value"] == "United Kingdom"
