51953772cd5c09c7fbce626046677b74c55ca2c3
kbauch <karine.bauch.web@gmail.com>
2024-09-17 11:46:52 +0200

    use httpx instead of requests

--- src/services/meteo.py
+++ src/services/meteo.py
@@ -1,11 +1,11 @@
-import requests
+import httpx
 
 weather_api = "https://wttr.in/"
 
 
 def get_weather(location: str) -> dict:
     weather_url = f"{weather_api}{location}?format=j1"
-    return requests.get(weather_url).json()
+    return httpx.get(weather_url).json()
 
 
 if __name__ == "__main__":
