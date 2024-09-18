9e509c3d717cbaed0bfd6a1c9a0ea2bcd58af382
kbauch <karine.bauch.web@gmail.com>
2024-09-18 13:21:31 +0200

    Add style

--- src/cli/main.py
+++ src/cli/main.py
@@ -12,11 +12,18 @@ def recipe(location: str):
     """
     Send a recipe weather-appropriate for a LOCATION
     """
-    rich.print(f"We're searching a [bold green]fantastic[/bold green] recipe :fork_and_knife_with_plate: for you at {location}, please wait...\n")
+    rich.print(
+        f"We're searching a [bold green]fantastic[/bold green] recipe :fork_and_knife_with_plate: for you at {location}, "
+        f"please wait..."
+    )
 
-    with rich.progress.Progress(rich.progress.SpinnerColumn(spinner_name='aesthetic', speed=0.5), transient=True) as progress:
+    with rich.progress.Progress(
+        rich.progress.SpinnerColumn(spinner_name="aesthetic", speed=0.5), transient=True
+    ) as progress:
         progress.add_task("Waiting for your recipe...", start=True)
         recipe_ = services.recipe.get_recipe(location)
+
+    rich.print("[bold cyan]Here is your recipe. Enjoy![/bold cyan]")
     rich.print(recipe_)
 
 
