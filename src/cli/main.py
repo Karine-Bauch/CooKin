b17b9f8c02ce718e7150dc5ec66d544b6e3e916e
kbauch <karine.bauch.web@gmail.com>
2024-09-18 12:19:41 +0200

    delete unused command + ruff

--- src/cli/main.py
+++ src/cli/main.py
@@ -6,14 +6,14 @@ app = typer.Typer()
 
 
 @app.command()
-def hello(name: str):
-    print(f"Hello {name}")
-
-
-@app.command()
 def recipe(location: str):
+    """
+    Send a recipe weather-appropriate for a LOCATION
+    """
+    print("We're searching a fantastic recipe for you, please wait...")
     recipe_ = services.recipe.get_recipe(location)
     print(recipe_)
+
 
 if __name__ == "__main__":
     app()
