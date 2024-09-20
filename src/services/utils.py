a71290f8e4e651b2dfbb31b018c051d07239e3ea
kbauch <karine.bauch.web@gmail.com>
2024-09-20 14:43:41 +0200

    Fix and refacto retry weather api

--- src/services/utils.py
+++ src/services/utils.py
@@ -1,14 +1,18 @@
+import contextlib
 import time
 import typing
+
+import httpx
 
 INCREMENTAL_WAIT = [1, 3, 5, 8, 13]
 
 
 def retry(api_call: typing.Callable, predicate: typing.Callable, *args, **kwargs):
     for wait in INCREMENTAL_WAIT:
-        weather = api_call(*args, **kwargs)
-        if predicate(weather):
-            return weather
+        with contextlib.suppress(httpx.TimeoutException):
+            result = api_call(*args, **kwargs)
+            if predicate(result):
+                return result
         print(f"Retry, waiting {wait} seconds")
         time.sleep(wait)
     raise TimeoutError
