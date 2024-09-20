0d4c4f9530b5eae074a72ba0e8db210ea85c9efb
kbauch <karine.bauch.web@gmail.com>
2024-09-20 17:06:11 +0200

    typing

--- src/services/utils.py
+++ src/services/utils.py
@@ -8,11 +8,12 @@ import openai
 INCREMENTAL_WAIT = [1, 3, 5, 8, 13]
 
 
-def retry(api_call: typing.Callable, predicate: typing.Callable, *args, **kwargs):
+def retry(api_call: typing.Callable, predicate: typing.Callable, *args, **kwargs) -> dict:
     for wait in INCREMENTAL_WAIT:
         with contextlib.suppress(httpx.TimeoutException, openai.APITimeoutError):
             result = api_call(*args, **kwargs)
             if predicate(result):
+                print(type(result))
                 return result
         print(f"Retry, waiting {wait} seconds")
         time.sleep(wait)
