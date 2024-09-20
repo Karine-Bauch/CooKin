d3a772043e6a7c42728fa820daf7e3918d708a74
kbauch <karine.bauch.web@gmail.com>
2024-09-20 17:02:30 +0200

    Refacto: refacto, simplifying

--- src/services/weather.py
+++ src/services/weather.py
@@ -19,17 +19,16 @@ def check_city(city) -> None:
 def weather_api_call(location) -> dict:
     check_city(location)
     weather_url = f"{weather_api}{location}?format=j1"
-    return httpx.get(weather_url).json()
+    weather = httpx.get(weather_url)
+    return weather.json()
 
 
 def get_weather(location) -> dict:
-    return services.utils.retry(
-            weather_api_call, lambda response: response, location
-        )
+    return services.utils.retry(weather_api_call, lambda response: response, location)
 
 
 if __name__ == "__main__":
-    print(check_city("London"))
+    # print(check_city("London"))
     print(get_weather("London"))
-    print(check_city("58913784137849318840134134"))
-    print(get_weather("58913784137849318840134134"))
+    # print(check_city("58913784137849318840134134"))
+    # print(get_weather("58913784137849318840134134"))
